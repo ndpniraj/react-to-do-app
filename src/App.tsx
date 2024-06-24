3ba6921c854afb33a15b1a6dc9bcafcdbf747e1b
ndpniraj <ndpniraj@gmail.com>
2024-06-24 16:40:26 +0530

    basic form ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,27 @@ import { FC } from "react";
 
 const App: FC = () => {
   return (
-    <div>
-      <p className="text-xl text-red-500 font-black">Hello React Developers</p>
+    <div className="flex justify-center items-center h-screen">
+      <form className="space-y-6 p-3 rounded shadow-md bg-white w-96">
+        <h1 className="text-2xl text-center">ToDo Form</h1>
+        <div>
+          <input
+            className="p-2 rounded outline-none border border-gray-500 w-full"
+            placeholder="Add Your ToDo Here"
+          />
+        </div>
+        <div>
+          <textarea
+            className="p-2 rounded outline-none border border-gray-500 w-full"
+            placeholder="Describe Your ToDo Here"
+          />
+        </div>
+        <div>
+          <button className="p-2 text-center rounded outline-none text-white bg-gray-500 w-full">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
