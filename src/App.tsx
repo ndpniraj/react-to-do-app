a8ec2ce0e374685fd8028505cca475c0f0595aa5
ndpniraj <ndpniraj@gmail.com>
2024-06-24 17:12:00 +0530

    custom component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,14 @@
-import { FC, useState } from "react";
+import { FC } from "react";
+import ToDoForm from "./components/ToDoForm";
 
 const App: FC = () => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-
   return (
     <div className="flex justify-center items-center h-screen">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log(title, description);
+      <ToDoForm
+        onSubmit={(data) => {
+          console.log(data);
         }}
-        className="space-y-6 p-3 rounded shadow-md bg-white w-96"
-      >
-        <h1 className="text-2xl text-center">ToDo Form</h1>
-        <div>
-          <input
-            name="title"
-            className="p-2 rounded outline-none border border-gray-500 w-full"
-            placeholder="Add Your ToDo Here"
-            value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <textarea
-            name="description"
-            className="p-2 rounded outline-none border border-gray-500 w-full"
-            placeholder="Describe Your ToDo Here"
-            value={description}
-            onChange={(e) => {
-              setDescription(e.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <button className="p-2 text-center rounded outline-none text-white bg-gray-500 w-full">
-            Submit
-          </button>
-        </div>
-      </form>
+      />
     </div>
   );
 };
---
+++ src/components/ToDoForm.tsx
@@ -0,0 +1,53 @@
+import { FC, useState } from "react";
+
+type Data = { title: string; description: string };
+
+interface Props {
+  onSubmit?(data: Data): void;
+}
+
+const ToDoForm: FC<Props> = ({ onSubmit }) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (onSubmit) onSubmit({ title, description });
+      }}
+      className="space-y-6 p-3 rounded shadow-md bg-white w-96"
+    >
+      <h1 className="text-2xl text-center">ToDo Form</h1>
+      <div>
+        <input
+          name="title"
+          className="p-2 rounded outline-none border border-gray-500 w-full"
+          placeholder="Add Your ToDo Here"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <textarea
+          name="description"
+          className="p-2 rounded outline-none border border-gray-500 w-full"
+          placeholder="Describe Your ToDo Here"
+          value={description}
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <button className="p-2 text-center rounded outline-none text-white bg-gray-500 w-full">
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default ToDoForm;
