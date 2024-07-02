e2250d47b6a2f39d7d40ccca976bbc94bde111c4
ndpniraj <ndpniraj@gmail.com>
2024-07-02 15:26:24 +0530

    home page refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import { Routes, Route } from "react-router-dom";
-import ToDoForm, { Data } from "./components/ToDoForm";
-import ToDoItem from "./components/ToDoItem";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 
 const App: FC = () => {
-  const [tasks, setTasks] = useState<Data[]>([]);
-
-  const myCustomTasks = tasks.map((item, index) => {
-    return <ToDoItem key={index} title={item.title} />;
-  });
-
-  const HomePage = () => (
-    <div className="flex justify-center items-center h-screen">
-      <ToDoForm
-        onSubmit={(data) => {
-          setTasks([...tasks, data]);
-        }}
-      />
-      <div className="ml-6">{myCustomTasks}</div>
-    </div>
-  );
-
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,26 @@
+import { FC, useState } from "react";
+import ToDoForm, { Data } from "../components/ToDoForm";
+import ToDoItem from "../components/ToDoItem";
+
+interface Props {}
+
+const Home: FC<Props> = () => {
+  const [tasks, setTasks] = useState<Data[]>([]);
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <ToDoForm
+        onSubmit={(data) => {
+          setTasks([...tasks, data]);
+        }}
+      />
+      <div className="ml-6">
+        {tasks.map((item, index) => {
+          return <ToDoItem key={index} title={item.title} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
